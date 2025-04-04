010c7068f267cbe512faf096a8200a77c1553c67
Nowaxial <Nowaxial@users.noreply.github.com>
2025-04-04 13:36:09 +0200

    Revert "Add UserAgePrice method and improve menu handling"
    This reverts commit 237d635d32586f9d4a0e1ca0d5a9a1d2330e7a39.

--- FlowControlExercise2Lexicon/Program.cs
+++ FlowControlExercise2Lexicon/Program.cs
@@ -9,7 +9,7 @@
             //TODO: 3. Lägga till en metod för att avsluta programmet.
             //TODO: 4. Lägga till en metod för att skriva ut en text 10 gånger.
             //TODO: 5. Lägga till en metod för att hämta det tredje ordet i en mening.
-            //DONE: 6. Lägga till en metod för att beräkna biljettpriser beroende på ålder.
+            //TODO: 6. Lägga till en metod för att beräkna biljettpriser beroende på ålder.
             //TODO: 7. Lägga till en metod för att beräkna biljettpriser beroende på sällskapets storlek och ålder
 
             bool isRunning = true;
@@ -19,7 +19,6 @@
                 //Här är huvudmenyn med de alternativ som ska kunna väljas senare
                 //Den körs i en oändlig loop tills användaren stänger ner programmet, loopen avslutas genom att sätta isRunning till false.
                 //Avslutar med en Console.ReadLine() för att vänta på användarens input och då det är en oändlig loop så kommer den att köra om menyn igen.
-
                 Console.WriteLine("============================================================");
                 Console.WriteLine("|                VÄLKOMMEN TILL HUVUDMENYN!                |");
                 Console.WriteLine("============================================================");
@@ -33,7 +32,6 @@
                 Console.WriteLine("0. Avsluta programmet\n");
 
                 Console.Write("Ditt val: ");
-
                 // Kommenterar ut Console.ReadLine(); då den bara var för att testa och se huvudmenyn hur den blev
 
                 string inputChoice = Console.ReadLine()!.ToUpper();
@@ -44,14 +42,9 @@
                         //Avslutar programmet genom att ändra isRunning till false
                         Console.WriteLine("Avslutar programmet...");
                         isRunning = false;
-
                         break;
 
                     case "1":
-
-                        UserAgePrice();  //Anropar metoden UserAgePrice()
-                        Console.WriteLine("Tryck på valfri tangent för att återgå till huvudmenyn.");
-                        Console.ReadLine();
                         break;
 
                     case "2":
@@ -66,33 +59,11 @@
 
                     default:
                         //Om användaren skriver något annat än 1-4 eller 0 så skrivs ett felmeddelande ut
-                        Console.WriteLine("Felaktigt val, försök igen.");
-                        Console.WriteLine("Tryck på valfri tangent för att återgå till huvudmenyn.");
-                        Console.ReadLine();
-                        Console.Clear(); //Rensar konsolen så att huvudmenyn kommer upp igen
+                        Console.WriteLine("Felaktigt val, försök igen.\n");
                         break;
                 }
             }
             while (isRunning);
-
-            //Metoden UserAgePrice() som räknar ut biljettpriser beroende på ålder
-            static void UserAgePrice()
-            {
-                Console.Write("Ange din ålder: ");
-                int userAge = int.Parse(Console.ReadLine()!);
-                if (userAge <= 19)
-                {
-                    Console.WriteLine("Ungdomspris: 80kr");
-                }
-                else if (userAge >= 65)
-                {
-                    Console.WriteLine("Pensionärspris: 90kr");
-                }
-                else
-                {
-                    Console.WriteLine("Standardpris: 120kr");
-                }
-            }
         }
     }
 }
