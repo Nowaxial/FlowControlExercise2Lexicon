1647f6c38490d7ce1e8bfd59a13c4cca11f86df1
Nowaxial <Nowaxial@users.noreply.github.com>
2025-04-04 10:05:24 +0200

    Add project files.

---
+++ FlowControlExercise2Lexicon/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FlowControlExercise2Lexicon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
